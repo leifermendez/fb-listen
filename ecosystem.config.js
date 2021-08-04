29720bd2a77926ba293fac37b3210cf91cc8d83e
Leifer Mendez <leifer33@gmail.com>
2021-08-04 12:34:17 +0200

    set mode cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,12 @@ module.exports = {
     apps: [
         {
             name: "fb-messenger",
-            script: "./src/app.js"
+            script: "./src/app.js",
+            watch: false,
+            max_memory_restart: '1000M',
+            exec_mode: "cluster",
+            instances: 1
+
         }
     ]
 }
